3630731a5abc3ce98a63a7329ec98bd5ad059eac
James Cox <jamesncox@yahoo.com>
2019-06-03 17:00:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,10 +7,10 @@ class Application
     
     if now.hour >= 12 
       resp.write "Good Morning!"
-    #else
+    else
       resp.write "Good Afternoon!"
-     
-     resp.finish
+    end
+    resp.finish
   end 
   
 end 
